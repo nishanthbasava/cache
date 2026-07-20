d2b4f469151506f61a7492a4b5528ea759152b2c
Nishanth Basava <nishanthrbasava@gmail.com>
2026-07-20 15:37:50 -0500

    scaffolded pages

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,14 @@
+import BottomNav from "@/components/BottomNav";
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <main className="flex-1">{children}</main>
+      <BottomNav />
+    </div>
+  );
+}
